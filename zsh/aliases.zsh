9cef7ae1c7c737a5e2ac38900d98393029b99276
James Vaughan <vaughanj10@gmail.com>
2016-01-20 13:32:28 -0500

    added some git aliases

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -1,11 +1,21 @@
 # GIT
 
-alias gs='git status'
-alias gd='git diff'
+alias g='git'
+
 alias ga='git add'
+alias gaa='git add .'
+
+alias gs='git status'
+
+alias gd='git diff'
+
 alias gc='git commit'
 alias gcm='git commit -m'
+
 alias gp='git push'
+
+alias gpu='git pull'
+alias gpr='git pull --rebase'
 
 # VIM
 
