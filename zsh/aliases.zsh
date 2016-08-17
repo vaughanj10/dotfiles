a4dc117b4d70b08f6b49fe9e5f659b0ae79a9bce
James Vaughan <vaughanj10@gmail.com>
2016-08-17 09:49:32 -0400

    Added surround.vim to vimrc, updated zsh aliases for git, added irssi config.

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -9,7 +9,7 @@ alias h='history 1'
 alias g='git'
 
 alias ga='git add'
-alias gaa='git add .'
+alias ga.='git add .'
 
 alias gs='git status'
 
@@ -17,8 +17,9 @@ alias gd='git diff'
 
 alias gc='git commit'
 alias gcm='git commit -m'
-
+alias gca='git commit --amend --no-edit'
 alias gp='git push'
+alias gpt='git push --tags origin master'
 
 alias gpu='git pull'
 alias gpr='git pull --rebase'
@@ -27,6 +28,8 @@ alias gl='git lg'
 
 alias gb='git branch'
 alias gbs='git branch | cut -c 3- | selecta | xargs git checkout'
+
+alias grc='git rebase --continue'
 
 # VIM
 
