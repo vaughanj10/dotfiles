c415fbbcd245a5ed0433e38d716c62eef6730355
James Vaughan <vaughanj10@gmail.com>
2016-10-26 15:38:12 -0400

    Updated neobundle packages.

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -14,6 +14,7 @@ alias ga.='git add .'
 alias gs='git status'
 
 alias gd='git diff'
+alias gds='git diff --staged'
 
 alias gc='git commit'
 alias gcm='git commit -m'
@@ -25,11 +26,17 @@ alias gpu='git pull'
 alias gpr='git pull --rebase'
 
 alias gl='git lg'
+alias gll='gl -n 5'
 
 alias gb='git branch'
 alias gbs='git branch | cut -c 3- | selecta | xargs git checkout'
 
 alias grc='git rebase --continue'
+
+alias gff='git stash && git pull --rebase && git stash pop'
+
+alias gco='git checkout'
+alias gcob='git checkout --branch'
 
 # VIM
 
